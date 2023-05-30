11872c9726776fcd8edb3fa68726eb113898c887
Santiago Bedoya <sarcila@octolabs.io>
2023-05-30 17:24:51 -0500

    feat: main window

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello, World")
+from tkinter import Tk
+
+
+class App:
+    def __init__(self) -> None:
+        self.t = Tk()
+
+    def render(self):
+        self.t.title("Graph Airport")
+        self.t.minsize(width=650, height=650)
+        
+
+        self.t.mainloop()
+
+app = App()
+app.render()
